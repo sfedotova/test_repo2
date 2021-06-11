4cea22a405e8ed75edda1864d7a4a9adde321911
Sofia Fedotova <Sofia.Fedotova@spirent.com>
2021-06-11 11:54:29 +0700

    Update

--- scripts/sleep_five.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import sys
-import time
-
-duration = 5
-
-print "[{}]: '{}' sleeping for {} seconds ...".format(time.ctime(), sys.argv[0], duration)
-time.sleep( duration )
-print "[{}]: '{}' done sleeping for {} seconds".format(time.ctime(), sys.argv[0], duration)
--- scripts/sleep_twenty.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import sys
-import time
-
-duration = 20
-
-print "[{}]: '{}' sleeping for {} seconds ...".format(time.ctime(), sys.argv[0], duration)
-time.sleep( duration )
-print "[{}]: '{}' done sleeping for {} seconds".format(time.ctime(), sys.argv[0], duration)
